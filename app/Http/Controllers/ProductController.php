25bb3b0ac446f2ce0150fdef22377559dd293eed
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-11-15 17:59:45 +0700

    add product update and delete routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-
 class ProductController extends Controller
 {
     public function create(Request $request)
@@ -17,11 +16,11 @@ class ProductController extends Controller
             'price' => 'required|numeric|min:1000',
             'stock' => 'required|numeric|min:1',
             'description' => 'required|string|max:255',
-            'photo_product' => 'required|image|mimes:jpg,jpeg,png,gif|max:5120' // max 5MB
+            'photo_product' => 'required|image|mimes:jpg,jpeg,png,gif|max:5120', // max 5MB
         ]);
 
-        //2.Handle File Upload
-        $path = $request->file('photo_product')->store('product','public');
+        // 2.Handle File Upload
+        $path = $request->file('photo_product')->store('product', 'public');
 
         // 3. Create Product
         $product = Product::create([
@@ -29,15 +28,14 @@ class ProductController extends Controller
             'price' => $request->price,
             'stock' => $request->stock,
             'description' => $request->description,
-            'photo_product' => $path            
+            'photo_product' => $path,
         ]);
 
-         return response()->json([
+        return response()->json([
             'status' => 'success',
             'message' => 'Add Product Success',
-            'data' => $product
+            'data' => $product,
         ], 201);
-
     }
 
     public function index(Request $request)
@@ -45,12 +43,12 @@ class ProductController extends Controller
         // 1. Inisiasi Query Builder
         $query = Product::query();
 
-       // Filter: Pencarian berdasarkan Nama Produk
+        // Filter: Pencarian berdasarkan Nama Produk
         if ($request->has('name')) {
-            $query->where('name', 'like', '%' . $request->input('name') . '%');
+            $query->where('name', 'like', '%'.$request->input('name').'%');
         }
 
-       // Filter: Harga Minimum (min_price)
+        // Filter: Harga Minimum (min_price)
         if ($request->has('min_price')) {
             $minPrice = (float) $request->input('min_price');
             $query->where('price', '>=', $minPrice);
@@ -65,7 +63,7 @@ class ProductController extends Controller
         // --- 3. Terapkan Sorting ---
 
         // Tentukan kolom yang boleh diurutkan (whitelist)
-        $allowedSorts = ['id','name', 'price', 'stock', 'created_at'];
+        $allowedSorts = ['id', 'name', 'price', 'stock', 'created_at'];
 
         $sortBy = $request->input('sort_by'); // Kolom untuk mengurutkan
         $orderBy = $request->input('order_by', 'asc'); // Urutan (default ASC)
@@ -82,78 +80,93 @@ class ProductController extends Controller
             $query->orderBy('created_at', 'desc');
         }
 
-
         // --- 4. Terapkan Pagination ---
 
         $maxLimit = 100;
-        $limit = min((int)$request->input('limit', 15), $maxLimit);
+        $limit = min((int) $request->input('limit', 15), $maxLimit);
 
         // Lakukan pagination dan eksekusi query
         $products = $query->paginate($limit);
 
-
         return response()->json([
             'status' => 'success',
             'message' => 'Get All Product Success',
-            'data' => $products
-        ],200);
+            'data' => $products,
+        ], 200);
     }
 
     public function show($id)
     {
         $product = Product::find($id);
-        if (!$product){
-             return response()->json([
-            'status' => 'error',
-            'message' => 'Product Not Found'
-        ],404);
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product Not Found',
+            ], 404);
         }
 
         return response()->json([
             'status' => 'success',
             'message' => 'Get All Product Success',
-            'data' => $product
-        ],200);
+            'data' => $product,
+        ], 200);
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
-        // find product by id
         $product = Product::find($id);
-        if (!$product){
-             return response()->json([
-            'status' => 'error',
-            'message' => 'Product Not Found'
-        ],404);
+
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product Not Found',
+            ], 404);
         }
 
-         // Validate Input
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:1000',
             'stock' => 'required|numeric|min:1',
             'description' => 'required|string|max:255',
-            'photo_product' => 'sometimes|image|mimes:jpg,jpeg,png,gif|max:5120'
+            'photo_product' => 'sometimes|image|mimes:jpg,jpeg,png,gif|max:5120',
         ]);
 
         if ($request->hasFile('photo_product')) {
-            // Hapus file lama jika ada
+            // Hapus foto lama
             if ($product->photo_product && Storage::disk('public')->exists($product->photo_product)) {
-            Storage::disk('public')->delete($product->photo_product);
+                Storage::disk('public')->delete($product->photo_product);
             }
-            // Upload file baru
+
+            // Upload foto baru
             $path = $request->file('photo_product')->store('product', 'public');
             $validatedData['photo_product'] = $path;
         }
 
         $product->update($validatedData);
-        
+
         return response()->json([
             'status' => 'success',
             'message' => 'Update Product Success',
-            'data' => $product
+            'data' => $product,
         ], 200);
+    }
 
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product Not Found',
+            ], 404);
+        }
 
+        // lakukan delete
+        $product->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product Deleted Successfully',
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-// Import Controllers
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
+// Import Controllers
 use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/logout', [AuthController::class, 'logout']);
 
     Route::get('/user/profile', [UserController::class, 'profile']);
     Route::post('/user/update', [UserController::class, 'update']);
 
     Route::post('/addproduct', [ProductController::class, 'create']);
+    Route::post('/product/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -27,6 +27,3 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
-
-Route::post('/product/{id}', [ProductController::class, 'update']);
-
