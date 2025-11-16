0471ebcfbf10dc9878769f90f5a7beba75e2c7b5
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-11-16 08:53:19 +0700

    refactor: move ProductController to Api namespace and clean up imports

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
-// Import Controllers
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
